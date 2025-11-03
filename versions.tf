4e38d8e701c950aac5298879878da7fdddd9199a
Diamantino Almeida <djalmeida@sapo.pt>
2025-11-03 17:08:58 +0000

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,23 @@
+# Terraform and Provider Version Constraints
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.80"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.45"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
+  }
+}
